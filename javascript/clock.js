b0f2f4821bb05174e96b5b8303e3f367851f87fd
DR584 <deepakronport01@gmail.com>
2022-01-06 19:08:15 +0530

    A new clock

---
+++ javascript/clock.js
@@ -0,0 +1,12 @@
+
+  function addZero(i) {
+  if (i<10) { i = "0" + i
+  }
+  return i;
+  }
+  const d = new Date();
+  let h = addZero(d.getHours());
+  let m = addZero(d.getMinutes());
+  let s = addZero(d.getSeconds());
+  let time = h + ":" + m + ":" +s;
+  document.getElementById("demo").innerHTML = time;
